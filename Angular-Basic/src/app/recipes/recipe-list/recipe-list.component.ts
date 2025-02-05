d7c2af4cfcab23cbdf054f55f3925b8159cb2aa9
AfonsoMmsc <afonsonogueirammsc@gmail.com>
2025-02-05 00:22:51 +0000

    add header, recipes, and shopping list components with Bootstrap integration

---
+++ Angular-Basic/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
+
+@Component({
+  selector: 'app-recipe-list',
+  imports: [RecipeItemComponent, RecipeItemComponent, RecipeItemComponent],
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+
+}
---
+++ Angular-Basic/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+
+@Component({
+  selector: 'app-shopping-list',
+  imports: [ShoppingEditComponent, ShoppingEditComponent],
+  templateUrl: './shopping-list.component.html',
+  styleUrl: './shopping-list.component.css'
+})
+export class ShoppingListComponent {
+
+}
